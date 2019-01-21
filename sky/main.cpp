7fc0b81eded9c5fdb1b73808a705e31515126f1e
Alexandru R. Agrapine <alexandru@agrapine.com>
2019-01-21 18:06:21 +0000

    Initial Commit

---
+++ sky/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sky
+//
+//  Created by Alexandru R. Agrapine on 21/01/2019.
+//  Copyright © 2019 SwipeLab. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
